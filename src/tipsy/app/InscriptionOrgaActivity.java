274207140eef6d6123d5b5f4a9fd95523648fee7
Kef <valentin.quefelec@gmail.com>
2013-12-10 05:22:49 +0100

    Validation du formulaire d'inscription Orga et de connexion

--- src/tipsy/app/InscriptionOrgaActivity.java
+++ src/tipsy/app/InscriptionOrgaActivity.java
@@ -8,38 +8,38 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.CheckBox;
 import android.widget.Toast;
-/*
+
 import com.mobsandgeeks.saripaar.*;
 import com.mobsandgeeks.saripaar.annotation.*;
-*/
+
 import com.stackmob.sdk.callback.StackMobModelCallback;
 import com.stackmob.sdk.exception.StackMobException;
 import tipsy.commun.Organisateur;
 
-public class InscriptionOrgaActivity extends Activity {
+public class InscriptionOrgaActivity extends Activity implements Validator.ValidationListener {
 
-    //@Required(order = 2)
+    @Required(order = 2)
     private EditText    nom;
-    //@Required(order = 2)
-    //@Email(order = 3)
+    @Required(order = 2)
+    @Email(order = 3)
     private EditText    email;
-    //@Password(order = 3)
-    //@TextRule(order = 4, minLength = 6, message = "Entrez au moins 6 caractères.")
+    @Password(order = 3)
+    @TextRule(order = 4, minLength = 6, message = "Entrez au moins 6 caractères.")
     private EditText    password;
     private CheckBox    afficherPassword;
     private Button      inscription;
 
-    //private Validator   validator;
+    private Validator   validator;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription_orga);
-        /*
+
         validator = new Validator(this);
         validator.setValidationListener(this);
-        */
+
         nom                 = (EditText) findViewById(R.id.nom);
         email               = (EditText) findViewById(R.id.email);
         password            = (EditText) findViewById(R.id.password);
@@ -49,33 +49,44 @@ public class InscriptionOrgaActivity extends Activity {
         // TENTATIVE D'INSCRIPTION
         inscription.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //validator.validate();
-                // D'ABORD TENTATIVE DE CONNEXION EN TANT QU'ORGANISATEUR
-                Organisateur user = new Organisateur(
-                    email.getText().toString(),
-                    password.getText().toString(),
-                    nom.getText().toString()
-                );
-                user.save(new StackMobModelCallback() {
-                    @Override
-                    public void success() {
-                        Intent login = new Intent(InscriptionOrgaActivity.this, LoginActivity.class);
-                        startActivity(login);
-                    }
-
-                    // SINON TENTATIVE DE CONNEXION EN TANT QUE MEMBRE
-                    @Override
-                    public void failure(StackMobException e) {
-
-                    }
-                });
-
-
+                // Validation du formulaire d'inscription
+                validator.validate();
             };
         });
     }
-    /*
-    public void onValidationSucceeded() {}
+
+    public void onValidationSucceeded() {
+        // Création d'un nouvel organisateur
+        final Organisateur orga = new Organisateur(
+                email.getText().toString(),
+                password.getText().toString(),
+                nom.getText().toString()
+        );
+        // Sauvegarde dans la BDD
+        orga.save(new StackMobModelCallback() {
+            // Connexion automatique en cas de réussite
+            @Override
+            public void success() {
+                orga.login(new StackMobModelCallback() {
+                    @Override
+                    public void success() {
+                        // Tableau de bord Organisateur
+                        startActivity(new Intent(InscriptionOrgaActivity.this, HomeActivity.class));
+                    }
+                    @Override
+                    public void failure(StackMobException e) {
+                        // Connexion
+                        startActivity(new Intent(InscriptionOrgaActivity.this, LoginActivity.class));
+                    }
+                });
+            }
+
+            // En cas d'échec
+            @Override
+            public void failure(StackMobException e) {
+            }
+        });
+    }
 
     public void onValidationFailed(View failedView, Rule<?> failedRule) {
         String message = failedRule.getFailureMessage();
@@ -86,6 +97,6 @@ public class InscriptionOrgaActivity extends Activity {
         } else {
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
         }
-    }*/
+    }
 
 }
--- src/tipsy/app/LoginActivity.java
+++ src/tipsy/app/LoginActivity.java
@@ -11,15 +11,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.mobsandgeeks.saripaar.Rule;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.Required;
 import com.stackmob.sdk.callback.StackMobModelCallback;
 import com.stackmob.sdk.exception.StackMobException;
 
 import tipsy.commun.Organisateur;
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity  implements Validator.ValidationListener{
 
+    @Required(order=1)
+    @Email(order=2)
     private EditText    email;
+    @Required(order=3)
     private EditText    password;
     private Button      connect;
+    private TextView    inscription;
+
+    private Validator   validator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,30 +38,54 @@ public class LoginActivity extends Activity {
         connect  = (Button) findViewById(R.id.connect);
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
+        inscription = (EditText) findViewById(R.id.inscription);
+
+        validator = new Validator(this);
+        validator.setValidationListener(this);
 
         // TENTATIVE DE CONNEXION
         connect.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // D'ABORD TENTATIVE DE CONNEXION EN TANT QU'ORGANISATEUR
-                Organisateur user = new Organisateur(email.getText().toString(), password.getText().toString());
-                user.login(new StackMobModelCallback() {
-                    @Override
-                    public void success() {
-                        Intent home = new Intent(LoginActivity.this, HomeActivity.class);
-                        startActivity(home);
-                    }
-
-                    // SINON TENTATIVE DE CONNEXION EN TANT QUE MEMBRE
-                    @Override
-                    public void failure(StackMobException e) {
-                        Intent inscription = new Intent(LoginActivity.this, InscriptionOrgaActivity.class);
-                        startActivity(inscription);
-                    }
-                });
-
-
+                validator.validate();
             }
         });
+
+        // Redirection inscription
+        inscription.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, InscriptionOrgaActivity.class));
+            }
+        });
+    }
+
+
+    public void onValidationSucceeded() {
+        // D'ABORD TENTATIVE DE CONNEXION EN TANT QU'ORGANISATEUR
+        Organisateur user = new Organisateur(email.getText().toString(), password.getText().toString());
+        user.login(new StackMobModelCallback() {
+            @Override
+            public void success() {
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            }
+
+            // SINON TENTATIVE DE CONNEXION EN TANT QUE MEMBRE
+            @Override
+            public void failure(StackMobException e) {
+            }
+        });
+    }
+
+
+
+    public void onValidationFailed(View failedView, Rule<?> failedRule) {
+        String message = failedRule.getFailureMessage();
+
+        if (failedView instanceof EditText) {
+            failedView.requestFocus();
+            ((EditText) failedView).setError(message);
+        } else {
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
