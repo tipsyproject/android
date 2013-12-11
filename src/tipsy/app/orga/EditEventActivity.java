06b4b84e0d8d0c830600dbfca014c71e82d29ed8
kef- <valentin.quefelec@gmail.com>
2013-12-11 09:57:49 +0100

    Tabs Bar ajoutee

--- src/tipsy/app/orga/EditEventActivity.java
+++ src/tipsy/app/orga/EditEventActivity.java
@@ -1,5 +1,6 @@
 package tipsy.app.orga;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -10,7 +11,6 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 
 import tipsy.app.R;
 
@@ -18,9 +18,15 @@ import tipsy.app.R;
  * Created by Valoo on 05/12/13.
  */
 
-public class EditEventActivity extends FragmentActivity {
+public class EditEventActivity extends FragmentActivity implements ActionBar.TabListener {
     // Nombre de colonnes
     static final int NUM_ITEMS = 4;
+    private static final int[] icones = {
+            R.drawable.ic_action_about,
+            R.drawable.ic_action_place,
+            R.drawable.ic_action_go_to_today,
+            R.drawable.ic_action_settings
+    };
     // Gestionnaires de vues
     MyAdapter mAdapter;
     ViewPager mPager;
@@ -29,13 +35,50 @@ public class EditEventActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orga_editer_event);
+
+        final ActionBar actionBar = getActionBar();
+
         mAdapter = new MyAdapter(getSupportFragmentManager());
         mPager = (ViewPager)findViewById(R.id.event_pager);
         mPager.setAdapter(mAdapter);
+
+        actionBar.setHomeButtonEnabled(false);
+        // Specify that tabs should be displayed in the action bar.
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        for (int icon : icones) {
+            actionBar.addTab(actionBar.newTab().setIcon(icon).setTabListener(this));
+        }
+
+
+        mPager.setOnPageChangeListener(
+                new ViewPager.SimpleOnPageChangeListener() {
+                    @Override
+                    public void onPageSelected(int position) {
+                        // When swiping between pages, select the
+                        // corresponding tab.
+                        getActionBar().setSelectedNavigationItem(position);
+                    }
+                });
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, android.app.FragmentTransaction fragmentTransaction) {
+        mPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, android.app.FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, android.app.FragmentTransaction fragmentTransaction) {
+
     }
 
     public static class MyAdapter extends FragmentPagerAdapter {
-        private static final String[] titres = { "Description", "Localisation", "Date", "Paramètres" };
+
 
         public MyAdapter(FragmentManager fm) {
             super(fm);
@@ -60,11 +103,6 @@ public class EditEventActivity extends FragmentActivity {
                 default:
                     return EditEventSettingsFragment.init(position);
             }
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return titres[position];
         }
     }
 
--- src/tipsy/app/orga/HomeActivity.java
+++ src/tipsy/app/orga/HomeActivity.java
@@ -4,23 +4,23 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 import tipsy.app.R;
 
 /**
- * Created by tech on 05/12/13.
+ * Created by Valoo on 05/12/13.
  */
 public class HomeActivity extends Activity {
 
-    private Button buttonCreerEvent;
+    private ImageButton buttonCreerEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orga_home);
 
-        buttonCreerEvent = (Button) findViewById(R.id.button_creer_event);
+        buttonCreerEvent = (ImageButton) findViewById(R.id.button_creer_event);
 
         buttonCreerEvent.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
