17f0e954827ebbdf7adbe0550b7b1bbf804d09ea
kef- <valentin.quefelec@gmail.com>
2013-12-11 21:19:48 +0100

    Attention y'a du bug

--- src/tipsy/app/Help.java
+++ src/tipsy/app/Help.java
@@ -4,4 +4,5 @@ package tipsy.app;
  * Created by vquefele on 11/12/13.
  */
 public class Help {
+
 }
--- src/tipsy/commun/User.java
+++ src/tipsy/commun/User.java
@@ -1,5 +1,6 @@
 package tipsy.commun;
 
+import android.app.Activity;
 import android.content.Context;
 
 import com.stackmob.sdk.model.StackMobUser;
@@ -11,19 +12,13 @@ import java.io.FileOutputStream;
  */
 public abstract class User extends StackMobUser implements UtilisateurTipsy {
 
+    private static String lastUserFilename = "lastlogin";
+    private static boolean loggedIn = false;
     protected String nom = null;
 
     /* CONSTRUCTEUR */
     public User(String username, String password) {
         super(User.class, username, password);
-    }
-
-    /********************************
-    * Retourne le nom du fichier    *
-    * contenant les paramètres User *
-    ********************************/
-    public static String getConfigFilename(){
-        return "lastlogin";
     }
 
     public String getEmail(){
@@ -32,13 +27,24 @@ public abstract class User extends StackMobUser implements UtilisateurTipsy {
 
     public void remember(Context context){
         FileOutputStream outputStream;
-
         try {
-            outputStream = context.openFileOutput(getConfigFilename(), Context.MODE_PRIVATE);
+            outputStream = context.openFileOutput(lastUserFilename, Context.MODE_PRIVATE);
             outputStream.write(this.getEmail().getBytes());
             outputStream.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static String getLastUserFilename(Activity act){
+        
+    }
+
+    public static void loggingIn(){
+        User.loggedIn = true;
+    }
+
+    public static void loggingOut(){
+        User.loggedIn = false;
     }
 }
