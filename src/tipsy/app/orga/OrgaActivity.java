9d52a6bf75530dbaf440071d8cc09c3d5846d9e6
Kef <valentin.quefelec@gmail.com>
2013-12-22 23:26:10 +0100

    1

--- src/tipsy/app/orga/OrgaActivity.java
+++ src/tipsy/app/orga/OrgaActivity.java
@@ -2,62 +2,75 @@ package tipsy.app.orga;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 
-import com.stackmob.sdk.callback.StackMobModelCallback;
-import com.stackmob.sdk.callback.StackMobQueryCallback;
-import com.stackmob.sdk.exception.StackMobException;
-
-import java.util.List;
-
 import tipsy.app.HelpActivity;
-import tipsy.app.LoginActivity;
 import tipsy.app.R;
 import tipsy.app.TipsyApp;
 import tipsy.app.UserActivity;
-import tipsy.commun.Prefs;
-import tipsy.commun.User;
 
 /**
  * Created by Valoo on 05/12/13.
  */
-public class OrgaActivity extends UserActivity {
+public class OrgaActivity extends UserActivity implements OrgaListener{
 
     private TipsyApp app;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.act_user_menu);
+        setContentView(R.layout.act_user);
         super.onCreate(savedInstanceState);
         this.menu = new MenuOrga(this);
         menu.initAdapter(new UserActivity.DrawerItemClickListener());
 
         app = (TipsyApp) getApplication();
 
-        Log.d("TOUTAFAIT", app.getOrga().getEmail());
-
-        /*buttonCreerEvent = (ImageButton) findViewById(R.id.button_creer_event);
-
-        buttonCreerEvent.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(HomeOrgaActivity.this, EditEventActivity.class));*/
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, new HomeOrgaFragment());
+        ft.commit();
     }
-
 
     protected void selectItem(int position) {
 
-        if (position == MenuOrga.AIDE)
-            startActivity(new Intent(this, HelpActivity.class));
-        else if (position == MenuOrga.DECONNEXION) {
-            app.logout(this);
+        // Mise à jour du menu
+        this.menu.getDrawerList().setItemChecked(position, true);
+        setTitle(this.menu.getTitres_menu()[position]);
+        this.menu.getDrawerLayout().closeDrawer(this.menu.getDrawerList());
 
-        } else {
-            this.menu.getDrawerList().setItemChecked(position, true);
-            setTitle(this.menu.getTitres_menu()[position]);
-            this.menu.getDrawerLayout().closeDrawer(this.menu.getDrawerList());
+        // Action à réaliser en fonction de l'onglet
+        switch(position){
+            case MenuOrga.ACCUEIL:
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.content_frame, new HomeOrgaFragment());
+                ft.commit();
+                break;
+            case MenuOrga.AIDE:
+                startActivity(new Intent(this, HelpActivity.class));
+                break;
+            case MenuOrga.DECONNEXION:
+                app.logout(this);
+            break;
+            default:
+                break;
+
         }
 
+    }
+
+
+    // IMPLEMENTATIONS DES LISTENERS DE LA PARTIE ORGANISATEUR
+
+    public void onEventNew(){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, new EditEventFragment());
+        ft.commit();
+    }
+
+    public void onEventEdit(){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, new HomeOrgaFragment());
+        ft.commit();
     }
 
 }
