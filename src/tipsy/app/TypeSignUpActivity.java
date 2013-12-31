c91408d96f554045b46b5395c3930a508812b22d
alextoss <martinsalexandre95@gmail.com>
2013-12-31 17:57:52 +0100

    Bouton google connexion créé, ne marche pas (Pb de Permission ?)

--- src/tipsy/app/TypeSignUpActivity.java
+++ src/tipsy/app/TypeSignUpActivity.java
@@ -1,20 +1,67 @@
 package tipsy.app;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import tipsy.app.membre.SignUpMembreActivity;
 import tipsy.app.orga.SignUpOrgaActivity;
+import tipsy.commun.Membre;
+import tipsy.commun.User;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.plus.PlusClient;
+import com.mobsandgeeks.saripaar.Rule;
 
 /**
  * Created by fulgor on 23/12/13.
  */
-public class TypeSignUpActivity extends Activity {
+public class TypeSignUpActivity extends SignUpUser implements ConnectionCallbacks, OnConnectionFailedListener {
+    private static final int REQUEST_CODE_RESOLVE_ERR = 9000;
+
+    private ProgressDialog mConnectionProgressDialog;
+    private PlusClient mPlusClient;
+    private ConnectionResult mConnectionResult;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_typesignup);
+
+        mPlusClient = new PlusClient.Builder(this, this, this)
+                .setActions("http://schemas.google.com/AddActivity", "http://schemas.google.com/BuyActivity")
+                .setScopes(Scopes.PLUS_LOGIN)  // recommended login scope for social features
+                        // .setScopes("profile")       // alternative basic login scope
+                .build();
+        // Progress bar to be displayed if the connection failure is not resolved.
+        mConnectionProgressDialog = new ProgressDialog(this);
+        mConnectionProgressDialog.setMessage("Connexion en cours...");
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (v.getId() == R.id.sign_in_button && !mPlusClient.isConnected()) {
+                    if (mConnectionResult == null) {
+                        mConnectionProgressDialog.show();
+                    } else {
+                        try {
+                            mConnectionResult.startResolutionForResult(TypeSignUpActivity.this, REQUEST_CODE_RESOLVE_ERR);
+                        } catch (IntentSender.SendIntentException e) {
+                            // Try connecting again.
+                            mConnectionResult = null;
+                            mPlusClient.connect();
+                        }
+                    }
+                }
+            }
+        });
     }
 
     public void OnClickInscription(View view) {
@@ -29,4 +76,71 @@ public class TypeSignUpActivity extends Activity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mPlusClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mPlusClient.disconnect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (mConnectionProgressDialog.isShowing()) {
+            // The user clicked the sign-in button already. Start to resolve
+            // connection errors. Wait until onConnected() to dismiss the
+            // connection dialog.
+            if (result.hasResolution()) {
+                try {
+                    result.startResolutionForResult(this, REQUEST_CODE_RESOLVE_ERR);
+                } catch (IntentSender.SendIntentException e) {
+                    mPlusClient.connect();
+                }
+            }
+        }
+
+        // Save the intent so that we can start an activity when the user clicks
+        // the sign-in button.
+        mConnectionResult = result;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+        if (requestCode == REQUEST_CODE_RESOLVE_ERR && responseCode == RESULT_OK) {
+            mConnectionResult = null;
+            mPlusClient.connect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        final Membre membre = new Membre(
+                mPlusClient.getAccountName(),
+                null,
+                mPlusClient.getCurrentPerson().getName().getFamilyName(),
+                mPlusClient.getCurrentPerson().getName().getGivenName()
+        );
+        User.rememberMe(this, mPlusClient.getAccountName(), null);
+        signUpUser(membre);
+        Toast.makeText(this, mPlusClient.getAccountName() + " est connecté.", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onDisconnected() {
+        Log.d("Signup", "disconnected");
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+
+    }
+
+    @Override
+    public void onValidationFailed(View failedView, Rule<?> failedRule) {
+
+    }
 }
